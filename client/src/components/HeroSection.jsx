d68f10d9e7df10537c924ca86d0e3fec442f3e81
Charangandrothu <charangandrothu7@gmail.com>
2025-09-20 22:32:54 +0530

    Navbar completed and home page

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { ArrowRight, CalendarIcon, CameraIcon, ClockIcon, MonitorCheck, MonitorPlay, MoveIcon } from 'lucide-react'
+import backgroundImage from '../assets/backgroundimage.jpg' // <-- Import
+import { useNavigate } from 'react-router-dom'
+
+const HeroSection = () => {
+
+    const navigate=useNavigate();
+
+  return (
+    <div
+      className='flex flex-col items-start justify-center gap-4 px-6 md:px-16 lg:px-36 bg-cover bg-center h-screen'
+      style={{ backgroundImage: `url(${backgroundImage})` }} // <-- Use here
+    >
+      <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'>
+        They Call Him <br/> OG
+      </h1>
+      <div className='flex items-center gap-4 text-gray-300'>
+        <span>Action | Drama</span>
+        <div className='flex items-center gap-1'>
+          <CalendarIcon className='w-4.5 h-4.5'/>2025
+        </div>
+        <div className='flex items-center gap-1'>
+          <ClockIcon className='w-4.5 h-4.5'/>2h 29m
+        </div>
+      </div>
+      <p className='max-w-md text-gray-300'>OG revolves around a gangster named Ojas Gambheera 
+        who returns to Mumbai following his ten year disappearance, to kill another crime boss, Omi Bhau.</p>
+        <button onClick={() => navigate('/movies')}
+        className="flex items-center gap-2 px-6 py-3 bg-red-500 hover:bg-red-600 transition rounded-full font-medium cursor-pointer"
+        >
+        Explore Movies
+        <ArrowRight className="w-5 h-5" />
+        </button>
+        <button onClick={() => window.open('https://www.youtube.com/watch?v=7Y5q41D8_hs')}
+        className="flex items-center gap-2 px-6 py-3 bg-red-500 hover:bg-red-600 transition rounded-full font-medium cursor-pointer"
+        >
+        View Trailer
+        <MonitorPlay className="w-5 h-5" />
+        </button>
+    </div>
+    
+  )
+}
+
+export default HeroSection
